9e94fa7a7f85b8ecb44a6f7b521d7890d77c6aa8
ice-try <rubing_zheng@163.com>
2021-03-12 09:59:10 +0800

    规则ip和端口号可使用any

--- snort-system/panel/regex_utils.py
+++ snort-system/panel/regex_utils.py
@@ -16,6 +16,8 @@ def is_integer(num):
 # 校验是否是合法ip
 def is_ip(ip):
     ip_flag = True
+    if ip == "any":
+        return True
     # ip_pattern = re.compile(r'((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}')
     ip_list = str(ip).split('.')
     if len(ip_list) == 4:
@@ -34,6 +36,8 @@ def is_ip(ip):
 # 校验是否是合法端口号
 def is_port(port):
     port_flag = True
+    if port == "any":
+        return True
     if not is_integer(port) or int(port) < 0 or int(port) > 65535:
         port_flag = False
     return port_flag
